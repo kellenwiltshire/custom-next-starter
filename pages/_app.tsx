672b3faf5863c8d0140113b06cae4ef23f77833c
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-11-28 08:32:15 -0500

    Fix serving error. Project runs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,30 +12,31 @@ function handleExitComplete() {
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter();
-	return;
-	<>
-		<Head>
-			<meta charSet='utf-8' />
-			<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-			<meta
-				name='viewport'
-				content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
-			/>
-			<meta name='description' content='Description' />
-			<meta name='keywords' content='Keywords' />
-			<title>Next.js PWA Example</title>
+	return (
+		<>
+			<Head>
+				<meta charSet='utf-8' />
+				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+				<meta
+					name='viewport'
+					content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+				/>
+				<meta name='description' content='Description' />
+				<meta name='keywords' content='Keywords' />
+				<title>Next.js PWA Example</title>
 
-			<link rel='manifest' href='/manifest.json' />
-			<link href='/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-			<link href='/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-			<link rel='apple-touch-icon' href='/apple-icon.png'></link>
-			<meta name='theme-color' content='#317EFB' />
-		</Head>
-		<AnimatePresence exitBeforeEnter onExitComplete={handleExitComplete}>
-			<Component {...pageProps} key={router.route} />
-		</AnimatePresence>
-		;
-	</>;
+				<link rel='manifest' href='/manifest.json' />
+				<link href='/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+				<link href='/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+				<link rel='apple-touch-icon' href='/apple-icon.png'></link>
+				<meta name='theme-color' content='#317EFB' />
+			</Head>
+			<AnimatePresence exitBeforeEnter onExitComplete={handleExitComplete}>
+				<Component {...pageProps} key={router.route} />
+			</AnimatePresence>
+			;
+		</>
+	);
 }
 
 export default MyApp;
